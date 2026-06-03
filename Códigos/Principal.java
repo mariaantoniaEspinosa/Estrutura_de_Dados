f1195a3c7326cfe83563e2a7a16db555db37d7a8
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2026-06-03 10:57:26 -0300

    Add files via upload

---
+++ Códigos/Principal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList<String> estacoes = new ArrayList<>();
+
+        //populando estacoes
+        estacoes.add("a");
+        estacoes.add("b");
+        estacoes.add("c");
+        estacoes.add("d");
+        estacoes.add("e");
+
+        Grafo gAssimetrico = new Grafo(estacoes);
+        //a, b
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("a"), gAssimetrico.pegarIndice("b"));
+        //b, c 
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("b"), gAssimetrico.pegarIndice("c"));
+        //b, d
+       gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("b"), gAssimetrico.pegarIndice("d"));
+        //c, e
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("c"), gAssimetrico.pegarIndice("e"));
+        //d, a
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("d"), gAssimetrico.pegarIndice("a"));
+        //d, b
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("d"), gAssimetrico.pegarIndice("b"));
+        //d,c
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("d"), gAssimetrico.pegarIndice("c"));
+        //e, d
+        gAssimetrico.inserirAresta(gAssimetrico.pegarIndice("e"), gAssimetrico.pegarIndice("d"));
+
+
+        gAssimetrico.mostrarMatriz(); // curiosidade, não é tão utilizado
+
+        System.out.println();
+
+        gAssimetrico.mostrarGrafo();
+
+    }
+}
